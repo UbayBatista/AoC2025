b0bcb00ba999afca2780efd6dfff20ca3d3bc95f
UbayBatista <ubayantonio@gmail.com>
2026-06-29 16:29:26 +0200

    test: add failing tests for day 11 part A path counting and graph parsing logic

---
+++ src/test/java/software/aoc/day11/a/PathCounterTest.java
@@ -0,0 +1,45 @@
+package software.aoc.day11.a;
+
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import java.util.Map;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PathCounterTest {
+
+    @Test
+    public void should_count_all_valid_paths_from_start_to_end() {
+        Map<String, List<String>> graph = Map.of(
+                "you", List.of("bbb", "ccc"),
+                "bbb", List.of("ddd", "eee"),
+                "ccc", List.of("ddd", "eee", "fff"),
+                "ddd", List.of("ggg"),
+                "eee", List.of("out"),
+                "fff", List.of("out"),
+                "ggg", List.of("out"),
+                "hhh", List.of("ccc", "fff", "iii"),
+                "iii", List.of("out")
+        );
+
+        assertThat(PathCounter.countPaths(graph, "you", "out")).isEqualTo(5L);
+    }
+
+    @Test
+    public void should_return_zero_when_no_path_exists() {
+        Map<String, List<String>> graph = Map.of(
+                "you", List.of("bbb"),
+                "bbb", List.of("ccc")
+        );
+
+        assertThat(PathCounter.countPaths(graph, "you", "out")).isZero();
+    }
+
+    @Test
+    public void should_return_zero_when_start_node_is_missing() {
+        Map<String, List<String>> graph = Map.of(
+                "aaa", List.of("bbb")
+        );
+
+        assertThat(PathCounter.countPaths(graph, "you", "out")).isZero();
+    }
+}
