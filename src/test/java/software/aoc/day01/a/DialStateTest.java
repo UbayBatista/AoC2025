7ba93c0c2bb4b42f4cfe106321148652911b1306
UbayBatista <ubayantonio@gmail.com>
2026-07-01 03:47:07 +0200

    refactor: streamline day01 Part A and Part B test cases by removing temporary variables and inlining object instantiation

--- src/test/java/software/aoc/day01/a/DialStateTest.java
+++ src/test/java/software/aoc/day01/a/DialStateTest.java
@@ -7,25 +7,21 @@ public class DialStateTest {
 
     @Test
     public void should_rotate_right_without_wrapping() {
-        DialState state = new DialState(50, 0);
-        assertThat(state.next(10).position()).isEqualTo(60);
+        assertThat(new DialState(50, 0).next(10).position()).isEqualTo(60);
     }
 
     @Test
     public void should_rotate_left_without_wrapping() {
-        DialState state = new DialState(50, 0);
-        assertThat(state.next(-10).position()).isEqualTo(40);
+        assertThat(new DialState(50, 0).next(-10).position()).isEqualTo(40);
     }
 
     @Test
     public void should_wrap_around_when_rotating_right_past_maximum() {
-        DialState state = new DialState(95, 0);
-        assertThat(state.next(10).position()).isEqualTo(5);
+        assertThat(new DialState(95, 0).next(10).position()).isEqualTo(5);
     }
 
     @Test
     public void should_wrap_around_when_rotating_left_past_zero() {
-        DialState state = new DialState(5, 0);
-        assertThat(state.next(-10).position()).isEqualTo(95);
+        assertThat(new DialState(5, 0).next(-10).position()).isEqualTo(95);
     }
 }
--- src/test/java/software/aoc/day01/a/SafeDialTest.java
+++ src/test/java/software/aoc/day01/a/SafeDialTest.java
@@ -8,13 +8,11 @@ public class SafeDialTest {
 
     @Test
     public void should_calculate_correct_password_for_example_input() {
-        SafeDial safeDial = new SafeDial();
         List<String> instructions = List.of(
                 "L68", "L30", "R48", "L5", "R60",
                 "L55", "L1", "L99", "R14", "L82"
         );
 
-        int password = safeDial.calculatePassword(instructions);
-        assertThat(password).isEqualTo(3);
+        assertThat(new SafeDial().calculatePassword(instructions)).isEqualTo(3);
     }
 }
